1a8f7ec20cdda37910dd7ed4e0ce3e5c48013a22
RAFAL-PC WINDOWS <rapi2s@interia.pl>
2016-12-07 03:11:01 +0100

    powinno startować od 00:00 i co sekunde zmieniac sie o 00:01 itd az do 00:59 potem od nowa 00:00

--- test1/test1/test1.asm
+++ test1/test1/test1.asm
@@ -1,7 +1,24 @@
 
 .include "m32def.inc"
+//makra dla czytelnosci kodu
+.macro pushf
+		push Acc
+		in Acc,SREG
+		push Acc
+.endm
+
+.macro popf
+		pop Acc
+		out SREG, Acc
+		pop Acc
+.endm
 
 //definicje rejestrow
+.def licznik3 = r27
+.def licznik2 = r28
+.def licznik1 = r29
+.def Acc = R30
+.def Temp = R31
 .def Kolumna = R22
 .def Zerowanie = R21 
 .def Stos = R20
@@ -32,6 +49,7 @@ rjmp TimerOVF
 
 
 Reset:// przerwanie reset
+
 ldi Stos, LOW(RAMEND)
 out SPL, Stos
 ldi Stos, HIGH(RAMEND)
@@ -40,13 +58,26 @@ ldi Zerowanie, 0xff //inicjacje jak w instrukcji
 out DDRA, Zerowanie
 ldi Kolumna, 0xff
 out DDRD, Kolumna
-sei
+
+ldi Temp, (1<<CS00) | (1<<CS01)//to do przerwan
+out TCCR0, Temp //ustawienie preskalera na 64
+ldi Temp, 1<<TOIE0; //wlaczenie przerwania
+out TIMSK, Temp
+ldi Temp, 131 //ustawienie wart poczatkowej
+out TCNT0, Temp
+sei //wlaczenie przerwan
+ldi licznik1,0
+ldi licznik2,0
+ldi licznik3,0
+
 ldi Zerowanie, 0xFF//tu sobie ustawiamy zera- bedziemy tym wylaczac segmenty
-ldi Seg0, cztery
-ldi Seg0, trzy
-ldi Seg0, dwa
-ldi Seg3, jeden
+ldi Seg0, zero
+ldi Seg0, zero
+ldi Seg0, zero
+ldi Seg3, zero
 jmp Wyswietlanie
+
+
 
 Wyswietlanie: //petla glowna
 
@@ -88,5 +119,117 @@ brne L1
 nop
 
 
-TimerOVF:
+TimerOVF: //przerwanie czasowe
+pushf
+inc licznik1
+cpi licznik1, 125
+breq IncSekundyJ
+ContinueOVF:
+ldi Temp, 131 //ustawienie wart poczatkowej
+out TCNT0, Temp
+popf
 reti
+
+IncSekundyJ:
+ldi licznik1,0
+inc licznik2
+cpi licznik2, 1
+breq wartJedenJ
+cpi licznik2, 2
+breq wartDwaJ
+cpi licznik2, 3
+breq wartTrzyJ
+cpi licznik2, 4
+breq wartCzteryJ
+cpi licznik2, 5
+breq wartPiecJ
+cpi licznik2, 6
+breq wartSzescJ
+cpi licznik2, 7
+breq wartSiedemJ
+cpi licznik2, 8
+breq wartOsiemJ
+cpi licznik2, 9
+breq wartDziewiecJ
+cpi licznik2, 10
+breq wartDziesiecJ
+
+
+wartJedenJ:
+ldi Seg0, jeden
+rjmp ContinueOVF
+
+wartDwaJ:
+ldi Seg0, dwa
+rjmp ContinueOVF
+
+wartTrzyJ:
+ldi Seg0, trzy
+rjmp ContinueOVF
+
+wartCzteryJ:
+ldi Seg0, cztery
+rjmp ContinueOVF
+
+wartPiecJ:
+ldi Seg0, piec
+rjmp ContinueOVF
+
+wartSzescJ:
+ldi Seg0, szesc
+rjmp ContinueOVF
+
+wartSiedemJ:
+ldi Seg0, siedem
+rjmp ContinueOVF
+
+wartOsiemJ:
+ldi Seg0, osiem
+rjmp ContinueOVF
+
+wartDziewiecJ:
+ldi Seg0, dziewiec
+rjmp ContinueOVF
+
+wartDziesiecJ:
+ldi Seg0, zero
+ldi licznik2,0
+inc licznik3
+cpi licznik3, 1
+breq wartJedenD
+cpi licznik3, 2
+breq wartDwaD
+cpi licznik3, 3
+breq wartTrzyD
+cpi licznik3, 4
+breq wartCzteryD
+cpi licznik3, 5
+breq wartPiecD
+cpi licznik3, 6
+breq wartSzescD
+rjmp ContinueOVF
+
+wartJedenD:
+ldi Seg1, jeden
+rjmp ContinueOVF
+
+wartDwaD:
+ldi Seg1, dwa
+rjmp ContinueOVF
+
+wartTrzyD:
+ldi Seg1, trzy
+rjmp ContinueOVF
+
+wartCzteryD:
+ldi Seg1, cztery
+rjmp ContinueOVF
+
+wartPiecD:
+ldi Seg1, piec
+rjmp ContinueOVF
+
+wartSzescD:
+ldi Seg1, zero
+ldi licznik3,0
+rjmp ContinueOVF
